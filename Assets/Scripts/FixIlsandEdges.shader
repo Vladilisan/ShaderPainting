0016fc7b380c34cb5016ebc701d5faf4c016ccf7
Vladilisan <ilisanvladluca@yahoo.com>
2020-05-30 15:36:37 +0300

    Scripts

---
+++ Assets/Scripts/FixIlsandEdges.shader
@@ -0,0 +1,94 @@
+﻿Shader "Unlit/FixIlsandEdges"
+{
+	
+	SubShader
+	{
+		// =====================================================================================================================
+		// TAGS AND SETUP ------------------------------------------
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+		ZTest  Off
+		ZWrite Off
+		Cull   Off
+		Lighting Off
+		Fog { Mode Off }
+
+		Pass
+		{
+
+			
+		// =====================================================================================================================
+		// DEFINE AND INCLUDE ----------------------------------
+		    CGPROGRAM
+
+
+			#pragma  vertex   vert
+			#pragma  fragment frag
+			
+			#include "UnityCG.cginc"
+
+			// =====================================================================================================================
+			// DECLERANTIONS ----------------------------------
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv	  : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv	  : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+					sampler2D	_MainTex;
+			uniform	float4		_MainTex_TexelSize;
+					sampler2D   _IlsandMap;
+			
+			// =====================================================================================================================
+			// VERTEX FRAGMENT ----------------------------------
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv     = v.uv;
+				#if UNITY_UV_STARTS_AT_TOP
+				 if (_MainTex_TexelSize.y < 0)
+					o.uv.y = 1. - o.uv.y;
+				#endif
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+
+			fixed4 col = tex2D(_MainTex, i.uv);
+			half map  = tex2D(_IlsandMap,i.uv);
+			
+			half3 average = col;
+
+			if (map.x < .2) {									// only take an average if it is not in a uv ilsand
+				int n = 0;
+				average = half3(0., 0., 0.);
+
+				for (half x = -1.5; x <= 1.5; x++) {
+					for (half y = -1.5; y <= 1.5; y++) {
+
+						half3 c =  tex2Dlod(_MainTex, half4(i.uv + _MainTex_TexelSize*half2(x, y),0,0));
+						half  m =  tex2Dlod(_IlsandMap, half4(i.uv + _MainTex_TexelSize*half2(x, y),0,0));
+
+							   n += step(0.1, m);
+						 average += c * step(0.1, m);
+
+					}
+				}
+				average /= n;
+				col.xyz = average;
+			}
+		
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Scripts/TexturePaint.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TexturePaint : MonoBehaviour
+{
+
+    // ======================================================================================================================
+    // PARAMETERS -----------------------------------------------------------------------------------------------
+    public Texture baseTexture;                  // used to deterimne the dimensions of the runtime texture
+    public Material meshMaterial;                 // used to bind the runtime texture as the albedo of the mesh
+    public GameObject meshGameobject;
+    public Shader UVShader;                     // the shader usedto draw in the texture of the mesh
+    public Mesh meshToDraw;
+    public Shader ilsandMarkerShader;
+    public Shader fixIlsandEdgesShader;
+    public static Vector3 mouseWorldPosition;
+    public Camera mainCamera;
+    // --------------------------------
+    private RenderTexture markedIlsandes;
+    private CommandBuffer cb_markingIlsdands;
+    private int numberOfFrames;
+
+    // ---------------------------------
+    private PaintableTexture albedo;
+    // ======================================================================================================================
+    // INITIALIZE -------------------------------------------------------------------
+    void Start()
+    {
+        Vector4 mwp = new Vector4(100, 100, 100, 100);
+        Shader.SetGlobalVector("_Mouse", mwp);
+
+        // Texture and Mat initalization ---------------------------------------------
+        markedIlsandes = new RenderTexture(baseTexture.width, baseTexture.height, 0, RenderTextureFormat.R8);
+        albedo = new PaintableTexture(Color.white, baseTexture.width, baseTexture.height, "_MainTex", UVShader, meshToDraw, fixIlsandEdgesShader, markedIlsandes);
+
+
+        meshMaterial.SetTexture(albedo.id, albedo.runTimeTexture);
+
+        // Command buffer inialzation ------------------------------------------------
+
+        cb_markingIlsdands = new CommandBuffer();
+        cb_markingIlsdands.name = "markingIlsnads";
+
+
+        cb_markingIlsdands.SetRenderTarget(markedIlsandes);
+        Material mIlsandMarker = new Material(ilsandMarkerShader);
+        cb_markingIlsdands.DrawMesh(meshToDraw, Matrix4x4.identity, mIlsandMarker);
+        mainCamera.AddCommandBuffer(CameraEvent.AfterEverything, cb_markingIlsdands);
+
+
+        albedo.SetActiveTexture(mainCamera);
+
+        Shader.SetGlobalFloat("_BrushOpacity", 1);
+        Shader.SetGlobalColor("_BrushColor", Color.red);
+        Shader.SetGlobalFloat("_BrushSize", .13f);
+        Shader.SetGlobalFloat("_BrushHardness", .1f);
+
+        mainCamera.depthTextureMode = DepthTextureMode.Depth;
+
+        albedo.UpdateShaderParameters(meshGameobject.transform.localToWorldMatrix);
+    }
+    // ======================================================================================================================
+    // LOOP ---------------------------------------------------------------------------
+
+    private void Update()
+    {
+        if (numberOfFrames == 3) mainCamera.RemoveCommandBuffer(CameraEvent.AfterEverything, cb_markingIlsdands);
+
+        numberOfFrames++;
+
+
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Vector4 mwp = Vector3.positiveInfinity;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.tag != "PaintObject")
+                return;
+            else
+                mwp = hit.point;
+
+
+            mwp.w = Input.GetMouseButton(0) ? 1 : 0;
+
+            mouseWorldPosition = mwp;
+            Shader.SetGlobalVector("_Mouse", mwp);
+
+        }
+    }
+
+
+    [System.Serializable]
+    public class PaintableTexture
+    {
+        public string id;
+        public RenderTexture runTimeTexture;
+        public RenderTexture paintedTexture;
+
+        public CommandBuffer cb;
+
+        private Material mPaintInUV;
+        private Material mFixedEdges;
+        public RenderTexture fixedIlsands;
+
+        public PaintableTexture(Color clearColor, int width, int height, string id,
+            Shader sPaintInUV, Mesh mToDraw, Shader fixIlsandEdgesShader, RenderTexture markedIlsandes)
+        {
+            this.id = id;
+
+            runTimeTexture = new RenderTexture(width, height, 0)
+            {
+                anisoLevel = 0,
+                useMipMap = false,
+                filterMode = FilterMode.Bilinear
+            };
+
+            paintedTexture = new RenderTexture(width, height, 0)
+            {
+                anisoLevel = 0,
+                useMipMap = false,
+                filterMode = FilterMode.Bilinear
+            };
+
+
+            fixedIlsands = new RenderTexture(paintedTexture.descriptor);
+
+            Graphics.SetRenderTarget(runTimeTexture);
+            GL.Clear(false, true, clearColor);
+            Graphics.SetRenderTarget(paintedTexture);
+            GL.Clear(false, true, clearColor);
+
+
+            mPaintInUV = new Material(sPaintInUV);
+            if (!mPaintInUV.SetPass(0)) Debug.LogError("Invalid Shader Pass: ");
+            mPaintInUV.SetTexture("_MainTex", paintedTexture);
+
+            mFixedEdges = new Material(fixIlsandEdgesShader);
+            mFixedEdges.SetTexture("_IlsandMap", markedIlsandes);
+            mFixedEdges.SetTexture("_MainTex", paintedTexture);
+
+            // ----------------------------------------------
+
+            cb = new CommandBuffer();
+            cb.name = "TexturePainting" + id;
+
+
+            cb.SetRenderTarget(runTimeTexture,0);
+            cb.DrawMesh(mToDraw, Matrix4x4.identity, mPaintInUV);
+
+            cb.Blit(runTimeTexture, fixedIlsands, mFixedEdges);
+            cb.Blit(fixedIlsands, runTimeTexture);
+            cb.Blit(runTimeTexture, paintedTexture);
+
+
+
+        }
+
+        public void SetActiveTexture(Camera mainC)
+        {
+            mainC.AddCommandBuffer(CameraEvent.AfterEverything, cb);
+        }
+
+        public void UpdateShaderParameters(Matrix4x4 localToWorld)
+        {
+            mPaintInUV.SetMatrix("mesh_Object2World", localToWorld); // Mus be updated every time the mesh moves, and also at start
+        }
+    }
+}
+
